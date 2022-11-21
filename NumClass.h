1b0dec14e43185a9433edda5307bf3a6a52c3be9
Michal <michalruth144@gmail.com>
2022-11-21 17:59:22 +0200

    change indents

--- NumClass.h
+++ NumClass.h
@@ -7,33 +7,33 @@
 //isPrime, isStrong
 
 
-/*
-*/
+    /*
+    */
 
 
 
 
-int isArmstrong(int);
+    int isArmstrong(int);
 
-/*
-*/
-int isPalindrome(int);
+    /*
+    */
+    int isPalindrome(int);
 
-/*
+    /*
 
-*/
+    */
 
-int isPrime(int);
-
-
-/*Strong number is a special number whose sum of the factorial of digits is equal to the original
-number. For Example: 145 is strong number
-1! + 4! + 5! = 145
-*/
+    int isPrime(int);
 
 
+    /*Strong number is a special number whose sum of the factorial of digits is equal to the original
+    number. For Example: 145 is strong number
+    1! + 4! + 5! = 145
+    */
 
 
-int isStrong(int);
 
-#endif /*_NUMCLASS_H_*/
+
+    int isStrong(int);
+
+    #endif /*_NUMCLASS_H_*/
