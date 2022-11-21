ad1d0200315d16a1c0d0b608dd6497c44b698479
Michal <michalruth144@gmail.com>
2022-11-21 16:56:20 +0200

    fix declerations

--- NumClass.h
+++ NumClass.h
@@ -2,17 +2,38 @@
 // Created by michal on 13/11/22.
 //
 
-
+#ifndef _NUMCLASS_H_
+#define _NUMCLASS_H_
 //isPrime, isStrong
 
-int isPrime(int num)
 
-int isStrong(int num)
-
-int isArmstrong(int num)
-
-int isPalindrom(int num)
+/*
+*/
 
 
 
 
+int isArmstrong(int);
+
+/*
+*/
+int isPalindrome(int);
+
+/*
+
+*/
+
+int isPrime(int);
+
+
+/*Strong number is a special number whose sum of the factorial of digits is equal to the original
+number. For Example: 145 is strong number
+1! + 4! + 5! = 145
+*/
+
+
+
+
+int isStrong(int);
+
+#endif /*_NUMCLASS_H_*/
