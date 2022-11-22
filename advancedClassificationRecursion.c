978d8387096efed5104162c8093fb42771101207
Michal <michalruth144@gmail.com>
2022-11-22 22:25:08 +0200

    update and fix function isArmstrong

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -5,49 +5,47 @@
 // isArmstrong, isPalindrome
 
 
-//power - calculates the digit's power in recursion
-int calcPower_(int digit, int power){
-    if (power==1)
-        return digit;
-    return digit*(calcPower_(digit,power-1));
 
-}
-
- int countDigits_(int num){
-    int count =0;
-    while (num>0){
-        count=count+1;
-        num=num/10;
-    }
-    return count;
-}
+extern int calcPower(int digit, int power); 
+extern int countDigits(int num); 
+extern int armstrong(int num, int numDigits);
 
 
 
 // isArmstrong -  recursion
 
- ///this function checks if a number is an armstrong number recursively
-
+ ///this function calls another function, armstrong(int), which checks if a number is an armstrong number recursively
 
 int isArmstrong(int num){
-    if (num==0)
-    {
-
+    int ans=armstrong(num,countDigits(num));
+    if(ans==num){
+        return 1;
+    }
+    else{
         return 0;
     }
-    return calcPower_(num%10,countDigits_(num))+isArmstrong(num/10) ;
+}
+
+// int isArmstrong(int num){
+//     if (num==0)
+//     {
+
+//         return 0;
+//     }
+//     return calcPower(num%10,countDigits(num))+isArmstrong(num/10) ;
     
 
-}
+// }
+
 
  ///  this function checks if a number is palindrom recursively
  int isPalindrome(int num){
-    if(countDigits_(num)<2){
+    if(countDigits(num)<2){
         return 1;
     }
 
-    if(num%10 == num / (calcPower_(10,countDigits_(num)-1)))
-        return isPalindrome((num % calcPower_(10,countDigits_(num)-1 ) )/10);
+    if(num%10 == (num / (calcPower(10,countDigits(num)-1))))
+        return isPalindrome((num % calcPower(10,countDigits(num)-1 ) )/10); // taking down the first and last digits
     else
         return 0;    
 
@@ -58,5 +56,5 @@ int isArmstrong(int num){
 
 
 
-//  isPalindrome- recursion
+
 
