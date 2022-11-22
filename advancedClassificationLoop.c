13ae72fa0bf52c6481f3be28389bdae5547bfa83
Michal <michalruth144@gmail.com>
2022-11-22 22:25:08 +0200

    update and fix function isArmstrong

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -2,25 +2,9 @@
 // Created by michal on 13/11/22.
 //
 
-//power - calculates the digit's power in recursion
-int calcPower(int digit, int power){
-    if (power==1){
-        return digit;
-    }
-    return digit*calcPower(digit,power-1);
+extern int countDigits(int n); 
+extern int calcPower(int digit, int power); 
 
-}
-
-// countDigits - not neccesary
-
-int countDigits(int num){
-    int count =0;
-    while (num>0){
-        count=count+1;
-        num=num/10;
-    }
-    return count;
-}
 
 // isArmstrong, if the the num is equal to the sum of the nth powers of its digit.
 
@@ -31,7 +15,7 @@ int countDigits(int num){
     int original = num;
     int power=countDigits(num);
     int sumPower=0;
-    while (num>0){
+    while (num){
        sumPower= sumPower+ calcPower(num%10, power);
        num=num/10;
     }
@@ -58,7 +42,7 @@ int countDigits(int num){
  int isPalindrome(int num){
     int count=countDigits(num);
     int bool=1;
-    while((count/2)>0 && bool){
+    while((count/2) && bool){ // count/2>0
         if(num%10 != num/(calcPower(10,count-1)))
             bool=0;
         num= (num % calcPower(10,count-1) ) /10;  // remove the last and first digit of num
